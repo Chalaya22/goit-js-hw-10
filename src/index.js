49ea1d521deff1b94b0e79dd18ccc3c79af11fa0
Chalaya22 <chalayaolga22@gmail.com>
2023-09-12 18:14:03 +0300

    step 4

--- src/index.js
+++ src/index.js
@@ -8,14 +8,15 @@ const refs = {
   catInfo: document.querySelector('.cat-info'),
   loader: document.querySelector('.loader'),
 };
+// refs.loader.classList.replace('loader', 'is-hidden');
+// refs.catInfo.classList.add('is-hidden');
 
 refs.breedsSelect.addEventListener('change', createCatCard);
 
 fetchBreeds()
-  .then(breeds => {
-    console.log(breeds);
-    refs.breedsSelect.classList.remove('is-hidden');
-    selectBreeds(breeds, refs.breedsSelect);
+  .then(data => {
+    refs.loader.classList.remove('is-hidden');
+    refs.catInfo.classList.add('is-hidden');
     new SlimSelect({
       // з бібліотеки берем стиль селектора
       select: refs.breedsSelect,
@@ -32,13 +33,17 @@ fetchBreeds()
   })
   .finally(() => {
     refs.loader.classList.add('is-hidden');
+    refs.catInfo.classList.add('is-hidden');
   });
 
 function createCatCard(evt) {
+  refs.loader.classList.replace('is-hidden', 'loader');
+  refs.breedsSelect.classList.add('is-hidden');
   refs.catInfo.classList.add('is-hidden');
-  refs.loader.classList.remove('is-hidden');
   fetchCatByBreed(evt.currentTarget.value)
     .then(data => {
+      refs.loader.classList.replace('loader', 'is-hidden');
+      refs.breedsSelect.classList.remove('is-hidden');
       const { breeds, url } = data[0];
       const { name, description, temperament } = breeds[0];
       renderingCatInfo(url, name, description, temperament, refs.catInfo);
